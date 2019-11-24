28162189bc348b1eeeb5b331be3f12479c9f65db
ertugrulakash <57980538+ertugrulakash@users.noreply.github.com>
2019-11-24 10:21:11 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var ss =77;
+
+var express = require('express');
+var app = express();
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+
