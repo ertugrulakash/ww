5a35161912d219c63869482d543c7e6bfcd502b2
ertugrulakash <57980538+ertugrulakash@users.noreply.github.com>
2019-11-24 17:52:41 +0500

    Update pop.js

--- pop.js
+++ pop.js
@@ -1,25 +1,27 @@
 var cmd = require('node-cmd');
+var mainjson = process.env;
+
 
 console.log('i am pop.js');
+console.log('git update command will be executed in 5 minutes'); 
 
-setTimeout(function(){
-  console.log('i am called in 20 seconds'); 
+setTimeout(function(url){
   cmd.get(
-    `
-        git remote add origin https://github.com/ertugrulakash/ww.git
-        git fetch
-        git checkout origin/master app.js
-    `,
-    function(err, data, stderr){
-        if (!err) {
-          cmd.run('refresh');  // Refresh project
-          console.log("git updated with origin master");
-        } else {
-           console.log('error', err)
-        }
+      `
+          git remote add origin ${url}
+          git fetch
+          git checkout origin/master app.js
+      `,
+      function(err, data, stderr){
+          if (!err) {
+            cmd.run('refresh');  // Refresh project
+            console.log("git updated with origin master");
+          } else {
+             console.log('error', err)
+          }
 
-    }
-  );
-}, 20000);
+      }
+    );
+}, 20000, mainjson.giturl);
 
 
